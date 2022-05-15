6145ec818b0799aa8b98bf62e98047a4fa26a50c
SWL\daud <daud.cse@gmail.com>
2022-05-15 14:42:46 +0600

    Obj

---
+++ src/Daud.ApplicationCore/DTOs/RequestObj.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daud.ApplicationCore.DTOs
+{
+    public class RequestObj
+    {
+        public object Obj { get; set; }
+    }
+}
